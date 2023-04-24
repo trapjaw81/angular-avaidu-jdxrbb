425597ff1fe338b4fb23b7e7f206f37c6b185069
trapjaw81 <trapjaw81@gmail.com>
2023-04-24 16:20:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-avaidu-jdxrbb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-avaidu-jdxrbb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkDragDropSortingExample} from './cdk-drag-drop-sorting-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkDragDropSortingExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap: [CdkDragDropSortingExample],
+})
+export class AppModule {}
